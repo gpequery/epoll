8e8f76b8c307eeac54654b3c623a7fb42c66ad5a
pequery <gpequery@thomeurope.com>
2018-12-07 20:05:37 +0100

    externalise contract

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Start Project
 **[Linux]**  
-CMD 1 : Ganache-cli -a 40 -l 0x1C9C380  
+CMD 1 : ganache-cli -a 40 -l 0x1C9C380  
 CMD 2 : cd Truffle && truffle migrate --compile-all && cd ../Web && npm install && npm start
-  
+
 **[Windows]**  
-CMD 1 : Ganache-cli -a 40 -l 0x1C9C380  
+CMD 1 : ganache-cli -a 40 -l 0x1C9C380  
 CMD 2 : cd Truffle   
 CMD 3 : truffle migrate --compile-all  
 CMD 4 : cd ../Web  
 CMD 5 : npm install  
 CMD 6 : npm start  
-  
+
 http://localhost:8546/
 
 # epoll
--- Web/routes/index.js
+++ Web/routes/index.js
@@ -1,42 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var pollClass = new require('./pollContract');
 
 var contract = require('truffle-contract');
 var Web3 = require('web3');
 
-if (typeof web3 !== 'undefined') {
-    var web3 = new Web3(web3.currentProvider)
-} else {
-    var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-}
-
-var pollFactoryContract;
-
-const pollFactory = require('../../Truffle/build/contracts/PollFactory.json');
-
-pollFactoryContract = contract(pollFactory);
-
-pollFactoryContract.setProvider(web3.currentProvider);
-if (typeof pollFactoryContract.currentProvider.sendAsync !== "function") {
-    pollFactoryContract.currentProvider.sendAsync = function() {
-        return pollFactoryContract.currentProvider.send.apply(pollFactoryContract.currentProvider, arguments);
-    };
-}
-
-async function getMessage() {
-    let deployedContract = await pollFactoryContract.deployed();
-    var message = await deployedContract.getMessage();
-
-    return message;
-}
+var pollContract = new pollClass();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    getMessage().then(function(message) {
+    pollContract.getMessage()
+    .then( message => {
         res.render('index', { data: message });
-    }).catch(function(error) {
+    }).catch( (err) => {
         res.render('index', { data: error });
-    })
+    });
 });
 
 module.exports = router;
