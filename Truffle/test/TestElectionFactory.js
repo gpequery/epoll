4279918c0c2e6850bbc9303f697d842c92803444
JuanWick <jm.anghileri@wanadoo.fr>
2019-01-23 18:13:55 +0100

    [ADD] UnitTest

--- Truffle/test/TestElectionFactory.js
+++ Truffle/test/TestElectionFactory.js
@@ -1,5 +1,7 @@
 const ElectionFactory = artifacts.require("ElectionFactory");
 const assert = require('assert');
+// const Web3 = require('web3');
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
 
 contract("ElectionFactory", accounts => {
 
@@ -18,61 +20,47 @@ contract("ElectionFactory", accounts => {
         }).then ( result =>{
             assert.equal(result[0], true, "The state should be true");
             assert.equal(result[1], "Ok.", "The response should be OK");
-    });
-    });
+            // assert.equal(result[2], web3.toAscii(electionName), "The election name sould be the good one");
 
-    // it("should get empty election list", () => {
-    //
-    //     return ElectionFactory.deployed()
-    //         .then(instance => {
-    //             return instance.getElectionList();
-    //         }) .then(result => {
-    //             assert.equal(result[0], true, "The state should be true");
-    //             assert.equal(result[1], "Ok.", "The response should be OK");
-    //             assert.equal(result[2].length, 0, "The list should be empty");
-    //
-    //         });
-    // });
-    //
-    // it("should get 3 elections in list", () => {
-    //     let electionName = "Election_1";
-    //     let startCandidateDate = 1547774798;
-    //     let endCandidateDate = 1548259355;
-    //     let startVoteDate = 1548864155;
-    //     let endVoteDate = 1549036955;
-    //     return ElectionFactory.deployed()
-    //         .then(instance => {
-    //             instance.createElection.call(electionName, startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-    //             instance.createElection.call("Election_2", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-    //             instance.createElection.call("Election_3", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-    //             // let resultElection = instance.createElection.call("Election_4", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-    //             // instance.deleteElectionById(resultElection[2]);
-    //
-    //             return instance.getElectionsList.call();
-    //         }) .then(result => {
-    //             assert.equal(result[0], true, "The state should be true");
-    //             assert.equal(result[1], "Ok.", "The response should be OK");
-    //             assert.equal(result[2].length, 0, "The list should be empty");
-    //         });
-    // });
-
+        });
+    });
 });
 
-// function testGetElectionsList() public  {
-//        var (x1, y1, z1) = getElectionsList();
-//        Assert.equal(uint(z1.length), uint(1), "Should return 1 election");
-//
-//        Period memory candidaturePeriod = Period(125, 126);
-//        Period memory votePeriod = Period(150, 180);
-//
-//        createElection("Election_2", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-//        var (x3, y3, z3) = createElection("Election_4", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-//
-//        var (x2, y2, z2) = getElectionsList();
-//        Assert.equal(uint(z2.length), uint(3), "Should return 4 elections");
-//
-//        deleteElectionById(z3);
-//
-//        var (x4, y4, z4) = getElectionsList();
-//        Assert.equal(uint(z4.length), uint(2), "Should return 3 elections");
-//    }
+contract("ElectionFactory", accounts => {
+    it("should get empty election list", () => {
+
+        return ElectionFactory.deployed().then (async factory => {
+            return await factory.getElectionsList();
+        }).then ( result =>{
+            assert.equal(result[0], true, "The state should be true");
+            assert.equal(result[1], "Ok.", "The response should be OK");
+            assert.equal(result[2].length, 0, "The list should be empty");
+        });
+    });
+
+    it("should create 4 elections, delete one and return 3 in a list", () => {
+        let electionName = "Election_1";
+        let startCandidateDate = 1547774798;
+        let endCandidateDate = 1548259355;
+        let startVoteDate = 1548864155;
+        let endVoteDate = 1549036955;
+
+        return ElectionFactory.deployed().then (async factory => {
+            await factory.createElection(electionName, startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
+            await factory.createElection("Election_2", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
+            await factory.createElection("Election_3", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
+            await factory.createElection("Election_4", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
+            let electionList = await factory.getElectionsList();
+            await factory.deleteElectionById(electionList[2][0]);
+
+            return await factory.getElectionsList();
+        }).then ( result =>{
+
+            assert.equal(result[0], true, "The state should be true");
+            assert.equal(result[1], "Ok.", "The response should be OK");
+            assert.equal(result[2].length, 3, "The list should have 3 elections");
+        });
+    });
+
+
+});
