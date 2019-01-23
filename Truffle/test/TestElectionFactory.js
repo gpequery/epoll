b3a768bcbc1234bf2c9a2ca2d21a82d8bb4972eb
JuanWick <jm.anghileri@wanadoo.fr>
2019-01-23 18:50:25 +0100

    [ADD] UnitTest

--- Truffle/test/TestElectionFactory.js
+++ Truffle/test/TestElectionFactory.js
@@ -1,7 +1,16 @@
 const ElectionFactory = artifacts.require("ElectionFactory");
 const assert = require('assert');
-// const Web3 = require('web3');
-// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+const MSG_missingElection = "Election inconnu.";
+const MSG_missingCandidate = "Candidat inconnu.";
+const MSG_deleteCandidate = "Candidat supprimé.";
+const MSG_hasAlreadyVoted = "A déjà voté.";
+const MSG_hasVoted = "A voté.";
+const MSG_Ok = "Ok.";
+const MSG_NotOwner = "Vous n'êtes pas le propriétaire de cette éléction.";
+const MSG_wrongPeriod = "Ce n'est pas le bon moment.";
 
 contract("ElectionFactory", accounts => {
 
@@ -20,7 +29,7 @@ contract("ElectionFactory", accounts => {
         }).then ( result =>{
             assert.equal(result[0], true, "The state should be true");
             assert.equal(result[1], "Ok.", "The response should be OK");
-            // assert.equal(result[2], web3.toAscii(electionName), "The election name sould be the good one");
+            assert.equal(web3.utils.toUtf8(result[2]), electionName, "The election name sould be the good one");
 
         });
     });
@@ -62,5 +71,67 @@ contract("ElectionFactory", accounts => {
         });
     });
 
+    it("should get election by id", () => {
+        return ElectionFactory.deployed().then (async factory => {
+            let electionList = await factory.getElectionsList();
+            return await  factory.getElectionById(electionList[2][0]);
+        }).then ( result =>{
+            assert.equal(result[0], true, "The state should be true");
+            assert.equal(result[1], "Ok.", "The response should be OK");
+            assert.equal(web3.utils.toUtf8(result[2]), "Election_2", "The election name should be 'Election_2'");
+        });
+    });
+
+    it("should not get unknown election by id", () => {
+        return ElectionFactory.deployed().then (async factory => {
+            return await  factory.getElectionById(88);
+        }).then ( result =>{
+            assert.equal(result[0], false, "The state should be false");
+            assert.equal(result[1], MSG_missingElection, "The response should be : " +MSG_missingElection);
+        });
+    });
+
+    it("should get periods from election by id", () => {
+        let startCandidateDate = 1547774798;
+        let endCandidateDate = 1548259355;
+        let startVoteDate = 1548864155;
+        let endVoteDate = 1549036955;
+
+        return ElectionFactory.deployed().then (async factory => {
+            let electionList = await factory.getElectionsList();
+            return await  factory.getElectionPeriodById(electionList[2][0]);
+        }).then ( result =>{
+            assert.equal(result[0], true, "The state should be true");
+            assert.equal(result[1],"Ok.", "The response should be OK");
+            assert.equal(result[2],startCandidateDate,  "Should return the candidate start Date");
+            assert.equal(result[3],endCandidateDate,  "Should return the candidate end Date");
+            assert.equal(result[4],startVoteDate,  "Should return the period start Date");
+            assert.equal(result[5],endVoteDate,  "Should return the period end Date");
+        });
+    });
+
+    it("should not get unknown election period by id", () => {
+        return ElectionFactory.deployed().then (async factory => {
+            return await  factory.getElectionPeriodById(88);
+        }).then ( result =>{
+            assert.equal(result[0], false, "The state should be false");
+            assert.equal(result[1], MSG_missingElection, "The response should be : " +MSG_missingElection);
+        });
+    });
+
+    it("should add candidate", () => {
+        return ElectionFactory.deployed().then (async factory => {
+            return await  factory.addOrUpdateCandidate(88);
+        }).then ( result =>{
+            assert.equal(result[0], false, "The state should be false");
+            assert.equal(result[1], MSG_missingElection, "The response should be : " +MSG_missingElection);
+        });
+    });
 
 });
+
+// function testShouldAddCandidate() public {
+//        var (a1, b1) = addOrUpdateCandidate(electionId, bytes32("John"), candidateName, bytes32("Who will have to wash the dish"), bytes32("wwwwwwww"));
+//        Assert.equal(a1, true, "Should return true");
+//        Assert.equal(b1, "Ok.", "Should have added the candidate");
+//    }
--- Truffle/test/TestElectionFactory.sol
+++ Truffle/test/TestElectionFactory.sol
@@ -16,57 +16,7 @@ contract TestElectionFactory is ElectionFactory {
 //    uint endVoteDate = 1549036955;
 //
 //
-//    constructor() public {
-//    }
-//
-//    function testCreateElection() public {
-//        var (x, y, z) = createElection(electionName, startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-//        electionId = uint(z);
-//        Assert.equal(x, true, "The state should be true");
-//        Assert.equal(y, "Ok.", "The response should be OK");
-//    }
-//
-//    function testGetElectionsList() public  {
-//        var (x1, y1, z1) = getElectionsList();
-//        Assert.equal(uint(z1.length), uint(1), "Should return 1 election");
-//
-//        Period memory candidaturePeriod = Period(125, 126);
-//        Period memory votePeriod = Period(150, 180);
-//
-//        createElection("Election_2", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-//        var (x3, y3, z3) = createElection("Election_4", startCandidateDate, endCandidateDate, startVoteDate, endVoteDate);
-//
-//        var (x2, y2, z2) = getElectionsList();
-//        Assert.equal(uint(z2.length), uint(3), "Should return 4 elections");
-//
-//        deleteElectionById(z3);
-//
-//        var (x4, y4, z4) = getElectionsList();
-//        Assert.equal(uint(z4.length), uint(2), "Should return 3 elections");
-//    }
-//
-//    function testGetElectionById() public {
-//        var (a1, b1, c1, d1, e1) = getElectionById(electionId);
-//        Assert.equal(a1, true, "Should return true");
-//        Assert.equal(c1, electionName, "Should return the election name");
-//
-//        var (a10, b10, c10, d10, e10) = getElectionById(88);
-//        Assert.equal(a10, false, "Should return false");
-//        Assert.equal(b10, "Election inconnu.", "Should retuen error message 'Election inconnu.'");
-//    }
-//
-//    function testGetElectionPeriodById() public {
-//        var (f1, g1, h1, i1, j1, k1) = getElectionPeriodById(electionId);
-//        Assert.equal(f1, true, "Should return true");
-//        Assert.equal(h1, startCandidateDate, "Should return the candidate start Date");
-//        Assert.equal(i1, endCandidateDate, "Should return the candidate end Date");
-//        Assert.equal(j1, startVoteDate, "Should return the vote start Date");
-//        Assert.equal(k1, endVoteDate, "Should return the vote end Date");
-//
-//        var (f10, g20, h30, i40, j50, k50) = getElectionPeriodById(88);
-//        Assert.equal(f10, false, "Should return false");
-//        Assert.equal(g20, "Election inconnu.", "Should return error message 'Election inconnu.'");
-//    }
+
 //
 //    function testShouldAddCandidate() public {
 //        var (a1, b1) = addOrUpdateCandidate(electionId, bytes32("John"), candidateName, bytes32("Who will have to wash the dish"), bytes32("wwwwwwww"));
